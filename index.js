d1198b02049edce3aeb1b6b151c72428aac1bd61
Mhamad6000 <mhamadothman6000@gmail.com>
2022-10-26 20:57:22 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+const button = document.getElementById("button");
+function clickAlert() {
+  alert("I was clicked!");
+}
 function addingEventListener() {
+  button.addEventListener("click", clickAlert);
 }
